8532ebd2caee550d87ea34de48dfebf9cc19e0d6
Chaz Wilder <cwilder@chazwilder.onmicrosoft.com>
2024-10-28 05:10:38 -0400

    Editor Boiler

---
+++ app/admin/editor/page.tsx
@@ -0,0 +1,9 @@
+import BlogEditor from "@/components/admin/BlogEditor";
+
+export default function WritePage() {
+  return (
+    <div className="min-h-screen bg-background">
+      <BlogEditor />
+    </div>
+  );
+}
---
+++ app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen">
+      <h1>Hello Admin</h1>
+    </div>
+  );
+}
